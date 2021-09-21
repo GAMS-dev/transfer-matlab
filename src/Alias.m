35f471f90000b4362e8ec274c99a8fde278a8e66
Renke Kuhlmann <rkuhlmann@gams.com>
2021-09-21 11:09:33 +0200

    add set properties and methods to alias class and forward to alias_with set

--- src/Alias.m
+++ src/Alias.m
@@ -54,6 +54,41 @@ classdef Alias < handle
         alias_with
     end
 
+    properties (Dependent)
+        % description Aliased Set description
+        description
+
+        % is_singleton indicator if aliased Set is is_singleton
+        is_singleton
+
+        % dimension Dimension of aliased Set (in [0,20])
+        dimension
+
+        % size Shape of aliased Set (length == dimension)
+        size
+
+        % domain Domain of aliased Set (length == dimension)
+        domain
+
+        % domain_names Domain names of aliased Set
+        domain_names
+
+        % domain_labels Expected domain labels in records
+        domain_labels
+
+        % domain_info Specifies if domains are stored 'relaxed' or 'regular'
+        domain_info
+
+        % records Storage of aliased Set records
+        records
+
+        % format Format in which records are stored in
+        %
+        % If records are changed, this gets reset to 'unknown'. Calling isValid()
+        % will detect the format again.
+        format
+    end
+
     properties (Hidden, SetAccess = private)
         id
         container
@@ -133,9 +168,91 @@ classdef Alias < handle
             obj.alias_with = alias;
         end
 
+        function description = get.description(obj)
+            description = obj.alias_with.description;
+        end
+
+        function set.description(obj, description)
+            obj.alias_with.description = description;
+        end
+
+        function is_singleton = get.is_singleton(obj)
+            is_singleton = obj.alias_with.is_singleton;
+        end
+
+        function set.is_singleton(obj, is_singleton)
+            obj.alias_with.is_singleton = is_singleton;
+        end
+
+        function dimension = get.dimension(obj)
+            dimension = obj.alias_with.dimension;
+        end
+
+        function set.dimension(obj, dimension)
+            obj.alias_with.dimension = dimension;
+        end
+
+        function size_ = get.size(obj)
+            size_ = obj.alias_with.size;
+        end
+
+        function set.size(obj, size_)
+            obj.alias_with.size = size_;
+        end
+
+        function domain = get.domain(obj)
+            domain = obj.alias_with.domain;
+        end
+
+        function set.domain(obj, domain)
+            obj.alias_with.domain = domain;
+        end
+
+        function domain_names = get.domain_names(obj)
+            domain_names = obj.alias_with.domain_names;
+        end
+
+        function domain_labels = get.domain_labels(obj)
+            domain_labels = obj.alias_with.domain_labels;
+        end
+
+        function domain_info = get.domain_info(obj)
+            domain_info = obj.alias_with.domain_info;
+        end
+
+        function records = get.records(obj)
+            records = obj.alias_with.records;
+        end
+
+        function set.records(obj, records)
+            obj.alias_with.records = records;
+        end
+
+        function format_ = get.format(obj)
+            format_ = obj.alias_with.format;
+        end
+
     end
 
     methods
+
+        function setRecords(obj, varargin)
+            % Sets symbol records in supported format
+            %
+            % See also: GAMSTransfer.Symbol.setRecords
+            %
+
+            obj.alias_with.setRecords(varargin{:});
+        end
+
+        function transformRecords(obj, target_format)
+            % Transforms symbol records into given format
+            %
+            % See also: GAMSTransfer.Symbol.transformRecords
+            %
+
+            obj.alias_with.transformRecords(target_format);
+        end
 
         function valid = isValid(obj, varargin)
             % Checks correctness of alias
@@ -174,6 +291,221 @@ classdef Alias < handle
             end
 
             valid = true;
+        end
+
+        function dom_violations = getDomainViolations(obj)
+            % Get domain violations
+            %
+            % See also: GAMSTransfer.Symbol.getDomainViolations
+            %
+
+            dom_violations = obj.alias_with.getDomainViolations();
+        end
+
+        function resolveDomainViolations(obj)
+            % Extends domain sets in order to resolve domain violations
+            %
+            % See also: GAMSTransfer.Symbol.resolveDomainViolations
+            %
+
+            obj.alias_with.resolveDomainViolations();
+        end
+
+        function sparsity = getSparsity(obj)
+            % Returns the sparsity of symbol records
+            %
+            % See also: GAMSTransfer.Symbol.getSparsity
+            %
+
+            sparsity = obj.alias_with.getSparsity();
+        end
+
+        function card = getCardenality(obj)
+            % Returns the cardenality of symbol records
+            %
+            % See also: GAMSTransfer.Symbol.getCardenality
+            %
+
+            card = obj.alias_with.getCardenality();
+        end
+
+        function [value, where] = getMaxValue(obj, varargin)
+            % Returns the largest value in records
+            %
+            % See also: GAMSTransfer.Symbol.getMaxValue
+            %
+
+            [value, where] = obj.alias_with.getMaxValue(varargin{:});
+        end
+
+        function [value, where] = getMinValue(obj, varargin)
+            % Returns the smallest value in records
+            %
+            % See also: GAMSTransfer.Symbol.getMinValue
+            %
+
+            [value, where] = obj.alias_with.getMinValue(varargin{:});
+        end
+
+        function value = getMeanValue(obj, varargin)
+            % Returns the mean value over all values in records
+            %
+            % See also: GAMSTransfer.Symbol.getMeanValue
+            %
+
+            value = obj.alias_with.getMeanValue(varargin{:});
+        end
+
+        function [value, where] = getMaxAbsValue(obj, varargin)
+            % Returns the largest absolute value in records
+            %
+            % See also: GAMSTransfer.Symbol.getMaxAbsValue
+            %
+
+            [value, where] = obj.alias_with.getMaxAbsValue(varargin{:});
+        end
+
+        function n = countNa(obj, varargin)
+            % Returns the number of GAMS NA values in records
+            %
+            % See also: GAMSTransfer.Symbol.countNa
+            %
+
+            n = obj.alias_with.countNa(varargin{:});
+        end
+
+        function n = countUndef(obj, varargin)
+            % Returns the number of GAMS UNDEF values in records
+            %
+            % See also: GAMSTransfer.Symbol.countUndef
+            %
+
+            n = obj.alias_with.countUndef(varargin{:});
+        end
+
+        function n = countEps(obj, varargin)
+            % Returns the number of GAMS EPS values in records
+            %
+            % See also: GAMSTransfer.Symbol.countEps
+            %
+
+            n = obj.alias_with.countEps(varargin{:});
+        end
+
+        function n = countPosInf(obj, varargin)
+            % Returns the number of GAMS PINF (positive infinity) values in
+            % records
+            %
+            % See also: GAMSTransfer.Symbol.countPosInf
+            %
+
+            n = obj.alias_with.countPosInf(varargin{:});
+        end
+
+        function n = countNegInf(obj, varargin)
+            % Returns the number of GAMS MINF (negative infinity) values in
+            % records
+            %
+            % See also: GAMSTransfer.Symbol.countNegInf
+            %
+
+            n = obj.alias_with.countNegInf(varargin{:});
+        end
+
+        function nrecs = getNumberRecords(obj)
+            % Returns the number of GDX records (not available for matrix
+            % formats)
+            %
+            % See also: GAMSTransfer.Symbol.getNumberRecords
+            %
+
+            nrecs = obj.alias_with.getNumberRecords();
+        end
+
+        function nvals = getNumberValues(obj, varargin)
+            % Returns the number of values stored for this symbol.
+            %
+            % See also: GAMSTransfer.Symbol.getNumberValues
+            %
+
+            nvals = obj.alias_with.getNumberValues(varargin{:});
+        end
+
+        function uels = getUELs(obj, dim, varargin)
+            % Returns the UELs used in this symbol
+            %
+            % See also: GAMSTransfer.Symbol.getUELs
+            %
+
+            uels = obj.alias_with.getUELs(dim, varargin{:});
+        end
+
+        function uels = getUELLabels(obj, dim, ids)
+            % Returns the UELs labels for the given UEL IDs
+            %
+            % See also: GAMSTransfer.Symbol.getUELLabels
+            %
+
+            uels = obj.alias_with.getUELLabels(dim, ids);
+        end
+
+        function initUELs(obj, dim, uels)
+            % Sets the UELs without modifying UEL IDs in records
+            %
+            % See also: GAMSTransfer.Symbol.initUELs
+            %
+
+            obj.alias_with.initUELs(dim, uels);
+        end
+
+        function setUELs(obj, dim, uels)
+            % Sets the UELs with updating UEL IDs in records
+            %
+            % See also: GAMSTransfer.Symbol.setUELs
+            %
+
+            obj.alias_with.setUELs(dim, uels);
+        end
+
+        function addUELs(obj, dim, uels)
+            % Adds UELs to the symbol
+            %
+            % See also: GAMSTransfer.Symbol.addUELs
+            %
+
+            obj.alias_with.addUELs(dim, uels);
+        end
+
+        function removeUELs(obj, dim, uels)
+            % Removes UELs from the symbol
+            %
+            % See also: GAMSTransfer.Symbol.removeUELs
+            %
+
+            obj.alias_with.removeUELs(dim, uels);
+        end
+
+        function renameUELs(obj, dim, olduels, newuels)
+            % Renames UELs in the symbol
+            %
+            % See also: GAMSTransfer.Symbol.renameUELs
+            %
+
+            obj.alias_with.renameUELs(dim, olduels, newuels);
+        end
+
+    end
+
+    methods (Hidden)
+
+        function bool = isValidAsDomain(obj)
+            % Checks if alias could be used as a domain of a different symbol
+            %
+            % b = isValidAsDomain() returns true if this alias can be used as
+            % domain and false otherwise.
+            %
+
+            bool = obj.alias_with.isValidAsDomain();
         end
 
     end
