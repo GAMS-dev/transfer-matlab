e9ddbfd42fb5ece0314b62fe11643c62b2db0bba
Renke Kuhlmann <rkuhlmann@gams.com>
2022-03-28 15:24:55 +0000

    Windows Matlab CI Job

--- gams_transfer_test.m
+++ gams_transfer_test.m
@@ -38,7 +38,6 @@ function gams_transfer_test(varargin)
     addParameter(p, 'working_dir', tempname, is_string_char);
     addParameter(p, 'gams_dir', find_gams(), is_string_char);
     addParameter(p, 'exit_on_fail', false, @islogical);
-    addParameter(p, 'license', '', is_string_char);
     addParameter(p, 'only_default_config', false, @islogical);
     parse(p, varargin{:});
     if strcmp(p.Results.gams_dir, '')
@@ -51,6 +50,11 @@ function gams_transfer_test(varargin)
 
     % create working directory
     mkdir(working_dir);
+    filenames = cell(1, 8);
+    for i = 1:8
+        filenames{i} = fullfile(working_dir, sprintf('data%d.gdx', i));
+        copyfile(fullfile(current_dir, 'test', 'gdx', sprintf('data%d.gdx', i)), filenames{i});
+    end
     olddir = cd(working_dir);
 
     tic;
@@ -61,7 +65,7 @@ function gams_transfer_test(varargin)
         cfg = struct();
         cfg.working_dir = working_dir;
         cfg.gams_dir = gams_dir;
-        cfg.filenames = gams_transfer_test_create_gdx(cfg.gams_dir, cfg.working_dir, p.Results.license);
+        cfg.filenames = filenames;
         features = GAMSTransfer.Utils.checkFeatureSupport();
 
         % run tests
@@ -128,129 +132,4 @@ function gams_transfer_test(varargin)
     if p.Results.exit_on_fail && ~success
         exit(1);
     end
-end
-
-function gdx_filenames = gams_transfer_test_create_gdx(gams_dir, working_dir, license)
-
-    n_probs = 8;
-    gams_data = cell(1, n_probs);
-    gdx_filenames = cell(1, n_probs);
-
-    gams_data{1} = {
-    'Set i ''set_i'' / i1, i3 ''expl text 3'', i4, i6, i10 ''expl text 10''/;'
-    'Set j ''set_j'' / j2, j5 ''expl text 5'', j7 ''expl text 7'', j8 ''expl text 8'', j9 /;'
-    'Scalar a ''par_a'' / 4 /;'
-    'Parameter b(i) ''par_b'' / i1=1, i3=3, i10=10 /;'
-    'Positive Variable x(i,j) ''var_x'';'
-    'x.l(''i1'',''j2'') = 2; x.l(''i10'',''j7'') = 7; x.l(''i3'',''j9'') = 9;'
-    'x.up(''i6'',''j7'') = 30;'
-    'x.m(''i3'',''j8'') = 8; x.m(''i6'',''j5'') = 5;'
-    'execute_unloaddi ''data1.gdx'', i, j, a, b, x;'
-    };
-
-    gams_data{2} = {
-    'Scalar GUndef;'
-    'Scalar GNA / NA /;'
-    'Scalar GPInf / +Inf /;'
-    'Scalar GMInf / -Inf /;'
-    'Scalar GEps / eps /;'
-    'GUndef = 1/0;'
-    'ExecError = 0;'
-    'execute_unloaddi ''data2.gdx'', GUndef, GNA, GPInf, GMInf, GEps;'
-    };
-
-    gams_data{3} = {
-    'Set i / 1*10 /;'
-    'Set j / 1*2 /;'
-    'Alias(i,i2);'
-    'Alias(j,j2);'
-    'Parameter a(j) / 1=3, 2=4 /;'
-    'Variable x1(j);'
-    'Free Variable x2(j);'
-    'Binary Variable x3(j);'
-    'Integer Variable x4(j);'
-    'Positive Variable x5(j);'
-    'Negative Variable x6(j);'
-    'SOS1 Variable x7(j,i);'
-    'SOS2 Variable x8(j,i);'
-    'SemiInt Variable x9(j);'
-    'SemiCont Variable x10(j);'
-    'x1.l(''1'') = 1;'
-    'x2.l(''1'') = 1;'
-    'x3.l(''1'') = 1;'
-    'x4.l(''1'') = 1;'
-    'x5.l(''1'') = 1;'
-    'x6.l(''1'') = -1;'
-    'x7.l(''1'',i) = 1;'
-    'x8.l(''1'',i) = 1;'
-    'x9.l(''1'') = 1;'
-    'x10.l(''1'') = 1;'
-    'Equation e1(j) ''equ_1'';'
-    'Equation e2(j) ''equ_2'';'
-    'Equation e3(j) ''equ_3'';'
-    'e1(''1'').. x1(''1'') =e= 1;'
-    'e2(''1'').. x2(''1'') =g= 1;'
-    'e3(''1'').. x3(''1'') =l= 1;'
-    'execute_unloaddi ''data3.gdx'', i, j, i2, j2, a, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, e1, e2, e3;'
-    };
-
-    gams_data{4} = {
-    'Set i ''set_i'' / 1*5 /;'
-    'Set j ''set_j'' / 1*10 /;'
-    'Scalar a ''par_a'' / 4 /;'
-    'Parameter b(i) ''par_b'' / 1=1, 3=3, 5=5 /;'
-    'Parameter c(i,j) ''par_c'' / 1.6=16, 3.7=37, 4.9=49 /;'
-    'execute_unloadidx ''data4.gdx'', a, b, c;'
-    };
-
-    gams_data{5} = {
-    'Set i ''set_i'' / i1 ''i_elem_1'', i2, i3 ''i_elem_3'', i4, i5 ''i_elem_5'' /;'
-    'execute_unloaddi ''data5.gdx'', i;'
-    };
-
-    gams_data{6} = {
-    'Set i / i1 * i3 /;'
-    'Acronym small ''baby bear'', medium ''mama bear'', large ''papa bear'';'
-    'Parameter a(i) / i1 1, i2 medium, i3 large /;'
-    'execute_unloaddi ''data6.gdx'', i, a;'
-    };
-
-    gams_data{7} = {
-    'Set i ''set_i'' / i1, i2 /;'
-    'Alias (a,i);'
-    'Parameter b(a) ''par_b'' / i1=1, i2=2 /;'
-    'execute_unloaddi ''data7.gdx'', i, a, b;'
-    };
-
-    gams_data{8} = {
-    'Set i(i) ''set_i'' / i1, i2 /;'
-    'Parameter a(i) ''par_a'' / i1=1, i2=2 /;'
-    'execute_unloaddi ''data8.gdx'', i, a;'
-    };
-
-    for i = 1:n_probs
-        gams_exe = fullfile(gams_dir, 'gams');
-        gms_filename = fullfile(working_dir, sprintf('data%d.gms', i));
-        gdx_filenames{i} = fullfile(working_dir, sprintf('data%d.gdx', i));
-
-        fid = fopen(gms_filename, 'w');
-        if fid < 0
-            error('Can''t write file: %s', gms_filename);
-        end
-        for j = 1:numel(gams_data{i})
-            fprintf(fid, '%s\n', gams_data{i}{j});
-        end
-        fclose(fid);
-
-        cmd = [gams_exe, ' ', gms_filename];
-        if ~isempty(license)
-            cmd = sprintf('%s license=%s', cmd, license);
-        end
-        [rc, stdout] = system(cmd);
-        if rc
-            disp(stdout);
-            error('Can''t create GAMS test files.');
-        end
-    end
-
 end
---
+++ test/gdx/generate.m
@@ -0,0 +1,180 @@
+% Tests GAMSTransfer
+%
+
+%
+% GAMS - General Algebraic Modeling System Matlab API
+%
+% Copyright (c) 2020-2022 GAMS Software GmbH <support@gams.com>
+% Copyright (c) 2020-2022 GAMS Development Corp. <support@gams.com>
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the 'Software'), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+%
+
+function gams_transfer_testcreate(varargin)
+
+    current_dir = fileparts(mfilename('fullpath'));
+    addpath(fullfile(current_dir, '..', '..', 'test'));
+    addpath(fullfile(current_dir, '..', '..', 'src'));
+
+    p = inputParser();
+    is_string_char = @(x) (isstring(x) && numel(x) == 1 || ischar(x)) && ...
+        ~strcmpi(x, 'gams_dir');
+    addParameter(p, 'gams_dir', find_gams(), is_string_char);
+    addParameter(p, 'license', '', is_string_char);
+    parse(p, varargin{:});
+    if strcmp(p.Results.gams_dir, '')
+        error('GAMS system directory not found.');
+    end
+
+    % check paths
+    gams_dir = Utils.checkFilename(p.Results.gams_dir, '', false);
+    working_dir = tempname;
+
+    mkdir(working_dir);
+    olddir = cd(working_dir);
+
+    n_probs = 8;
+    gams_data = cell(1, n_probs);
+    gdx_filenames = cell(1, n_probs);
+
+    gams_data{1} = {
+    'Set i ''set_i'' / i1, i3 ''expl text 3'', i4, i6, i10 ''expl text 10''/;'
+    'Set j ''set_j'' / j2, j5 ''expl text 5'', j7 ''expl text 7'', j8 ''expl text 8'', j9 /;'
+    'Scalar a ''par_a'' / 4 /;'
+    'Parameter b(i) ''par_b'' / i1=1, i3=3, i10=10 /;'
+    'Positive Variable x(i,j) ''var_x'';'
+    'x.l(''i1'',''j2'') = 2; x.l(''i10'',''j7'') = 7; x.l(''i3'',''j9'') = 9;'
+    'x.up(''i6'',''j7'') = 30;'
+    'x.m(''i3'',''j8'') = 8; x.m(''i6'',''j5'') = 5;'
+    'execute_unloaddi ''data1.gdx'', i, j, a, b, x;'
+    };
+
+    gams_data{2} = {
+    'Scalar GUndef;'
+    'Scalar GNA / NA /;'
+    'Scalar GPInf / +Inf /;'
+    'Scalar GMInf / -Inf /;'
+    'Scalar GEps / eps /;'
+    'GUndef = 1/0;'
+    'ExecError = 0;'
+    'execute_unloaddi ''data2.gdx'', GUndef, GNA, GPInf, GMInf, GEps;'
+    };
+
+    gams_data{3} = {
+    'Set i / 1*10 /;'
+    'Set j / 1*2 /;'
+    'Alias(i,i2);'
+    'Alias(j,j2);'
+    'Parameter a(j) / 1=3, 2=4 /;'
+    'Variable x1(j);'
+    'Free Variable x2(j);'
+    'Binary Variable x3(j);'
+    'Integer Variable x4(j);'
+    'Positive Variable x5(j);'
+    'Negative Variable x6(j);'
+    'SOS1 Variable x7(j,i);'
+    'SOS2 Variable x8(j,i);'
+    'SemiInt Variable x9(j);'
+    'SemiCont Variable x10(j);'
+    'x1.l(''1'') = 1;'
+    'x2.l(''1'') = 1;'
+    'x3.l(''1'') = 1;'
+    'x4.l(''1'') = 1;'
+    'x5.l(''1'') = 1;'
+    'x6.l(''1'') = -1;'
+    'x7.l(''1'',i) = 1;'
+    'x8.l(''1'',i) = 1;'
+    'x9.l(''1'') = 1;'
+    'x10.l(''1'') = 1;'
+    'Equation e1(j) ''equ_1'';'
+    'Equation e2(j) ''equ_2'';'
+    'Equation e3(j) ''equ_3'';'
+    'e1(''1'').. x1(''1'') =e= 1;'
+    'e2(''1'').. x2(''1'') =g= 1;'
+    'e3(''1'').. x3(''1'') =l= 1;'
+    'execute_unloaddi ''data3.gdx'', i, j, i2, j2, a, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, e1, e2, e3;'
+    };
+
+    gams_data{4} = {
+    'Set i ''set_i'' / 1*5 /;'
+    'Set j ''set_j'' / 1*10 /;'
+    'Scalar a ''par_a'' / 4 /;'
+    'Parameter b(i) ''par_b'' / 1=1, 3=3, 5=5 /;'
+    'Parameter c(i,j) ''par_c'' / 1.6=16, 3.7=37, 4.9=49 /;'
+    'execute_unloadidx ''data4.gdx'', a, b, c;'
+    };
+
+    gams_data{5} = {
+    'Set i ''set_i'' / i1 ''i_elem_1'', i2, i3 ''i_elem_3'', i4, i5 ''i_elem_5'' /;'
+    'execute_unloaddi ''data5.gdx'', i;'
+    };
+
+    gams_data{6} = {
+    'Set i / i1 * i3 /;'
+    'Acronym small ''baby bear'', medium ''mama bear'', large ''papa bear'';'
+    'Parameter a(i) / i1 1, i2 medium, i3 large /;'
+    'execute_unloaddi ''data6.gdx'', i, a;'
+    };
+
+    gams_data{7} = {
+    'Set i ''set_i'' / i1, i2 /;'
+    'Alias (a,i);'
+    'Parameter b(a) ''par_b'' / i1=1, i2=2 /;'
+    'execute_unloaddi ''data7.gdx'', i, a, b;'
+    };
+
+    gams_data{8} = {
+    'Set i(i) ''set_i'' / i1, i2 /;'
+    'Parameter a(i) ''par_a'' / i1=1, i2=2 /;'
+    'execute_unloaddi ''data8.gdx'', i, a;'
+    };
+
+    for i = 1:n_probs
+        gams_exe = fullfile(gams_dir, 'gams');
+        gms_filename = fullfile(working_dir, sprintf('data%d.gms', i));
+        gdx_filenames{i} = fullfile(working_dir, sprintf('data%d.gdx', i));
+
+        % create GMS file
+        fid = fopen(gms_filename, 'w');
+        if fid < 0
+            error('Can''t write file: %s', gms_filename);
+        end
+        for j = 1:numel(gams_data{i})
+            fprintf(fid, '%s\n', gams_data{i}{j});
+        end
+        fclose(fid);
+
+        % create GDX file
+        cmd = [gams_exe, ' ', gms_filename];
+        if ~isempty(p.Results.license)
+            cmd = sprintf('%s license=%s', cmd, p.Results.license);
+        end
+        [rc, stdout] = system(cmd);
+        if rc
+            disp(stdout);
+            error('Can''t create GAMS test files.');
+        end
+
+        % copy GDX file into repository
+        copyfile(gdx_filenames{i}, fullfile(current_dir, sprintf('data%d.gdx', i)));
+    end
+
+    cd(olddir)
+
+end
